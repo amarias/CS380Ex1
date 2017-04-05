7ff9c4f9222ffb76e28fd909a0dc9ee179ff88ba
Allison Marias <amarias@cpp.edu>
2017-04-04 21:15:59 -0700

    Implemented multiple simultaneous clients.

--- EchoServer.java
+++ EchoServer.java
@@ -1,5 +1,6 @@
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -11,19 +12,29 @@ public final class EchoServer {
 	public static void main(String[] args) throws Exception {
 		try (ServerSocket serverSocket = new ServerSocket(22222)) {
 			while (true) {
-				try (Socket socket = serverSocket.accept()) {
-					String address = socket.getInetAddress().getHostAddress();
-					System.out.printf("Client connected: %s%n", address);
-					OutputStream os = socket.getOutputStream();
-					PrintStream out = new PrintStream(os, true, "UTF-8");
-					out.printf("Hi %s, thanks for connecting!%n", address);
+				try {
+					Socket socket = serverSocket.accept();
+					Runnable r = () -> {
+						try {
+							String address = socket.getInetAddress().getHostAddress();
+							System.out.printf("Client connected: %s%n", address);
+							OutputStream os = socket.getOutputStream();
+							PrintStream out = new PrintStream(os, true, "UTF-8");
+							out.printf("Hi %s, thanks for connecting!%n", address);
 
-					BufferedReader iBr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-					String text;
-					while (!(text = iBr.readLine()).equalsIgnoreCase("exit")) {
-						out.println(text);
-					}
-					System.out.printf("Client disconnected: %s%n", address);
+							BufferedReader iBr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+							String text;
+							while (!(text = iBr.readLine()).equalsIgnoreCase("exit")) {
+								out.println(text);
+							}
+							System.out.printf("Client disconnected: %s%n", address);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					};
+					new Thread(r).start();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
 			}
 		}
